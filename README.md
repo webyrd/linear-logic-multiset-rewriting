ec95f01a5bb2cc03c3a0d729830b651da0605565
William Byrd <webyrd@gmail.com>
2017-09-07 13:55:38 +0100

    updated readme

--- README.md
+++ README.md
@@ -8,7 +8,20 @@ Linear-logic multi-set term rewriting in miniKanren for storytelling!
 
 Inspired by the work of Chris Martens on 'Ceptre: A Language for Modeling Generative Interactive Systems'.
 
+TODO: Chris, please explain what is going onnnnnnnn!  :)
 
+
+A few of the rules for foxes and rabbits
+
+```
+[(fox hungry)] -> []                      :fox-die
+
+[((fox hungry) rabbit)] -> [(fox sated)]  :omnomnom
+
+[(fox sated)] -> [(fox hungry)]           :get-hungry
+```
+
+Two fox stories, involving two hoooongry foxes and two rabbits.  We require that at the end of the story there are at least three sated foxes and at least two rabbits:
 
 ```
 (run 2 (tr)
--- llmr.scm
+++ llmr.scm
@@ -69,6 +69,7 @@
        (== 'get-hungry name)])))
 |#
 
+#|
 (define rules
   (lambda (p q name)
     (conde
@@ -88,12 +89,33 @@
       [(== '((fox sated)) p)
        (== '((fox hungry)) q)
        (== 'get-hungry name)])))
+|#
+
+(define rules
+  (lambda (p q name)
+    (conde
+      [(== '((fox hungry)) p)
+       (== '() q)
+       (== 'fox-die name)]
+      [(== '((fox hungry) rabbit) p)
+       (== '((fox sated)) q)
+       (== 'omnomnom name)]
+      [(== '((fox sated)) p)
+       (== '((fox hungry)) q)
+       (== 'get-hungry name)]      
+      [(fresh (h1 h2)
+         (== `((fox ,h1) (fox ,h2)) p)
+         (== `((fox ,h1) (fox ,h2) (fox hungry)) q)
+         (== 'fox-multiply name))]
+      [(== '(rabbit rabbit) p)
+       (== '(rabbit rabbit rabbit rabbit) q)
+       (== 'rabbit-multiply name)])))
 
 (define step
   (lambda (delta delta^^ name)
     (fresh (p q delta^)
-      (split p delta delta^)
       (rules p q name)
+      (split p delta delta^)
       (appendo q delta^ delta^^))))
 
 (define step*
@@ -106,6 +128,44 @@
          (step* delta^ delta^^ tr))])))
 
 #!eof
+
+(run 2 (tr)
+  (let ((initial-state '((fox hungry) (fox hungry) rabbit rabbit)))
+    (fresh (final rest diff s s^ s2 s3 s4 r)
+      (== `(,initial-state . ,rest) tr)
+      (split `((fox sated) (fox sated) (fox sated) rabbit rabbit) final diff)
+      (step* initial-state final rest))))
+=>
+(((((fox hungry) (fox hungry) rabbit rabbit)
+   (rabbit-multiply
+    (rabbit rabbit rabbit rabbit (fox hungry) (fox hungry)))
+   (omnomnom ((fox sated) rabbit rabbit rabbit (fox hungry)))
+   (omnomnom ((fox sated) (fox sated) rabbit rabbit))
+   (rabbit-multiply
+    (rabbit rabbit rabbit rabbit (fox sated) (fox sated)))
+   (fox-multiply
+    ((fox sated) (fox sated) (fox hungry) rabbit rabbit rabbit
+     rabbit))
+   (omnomnom
+    ((fox sated) (fox sated) (fox sated) rabbit rabbit
+     rabbit))))
+ ((((fox hungry) (fox hungry) rabbit rabbit)
+   (rabbit-multiply
+    (rabbit rabbit rabbit rabbit (fox hungry) (fox hungry)))
+   (rabbit-multiply
+    (rabbit rabbit rabbit rabbit rabbit rabbit (fox hungry)
+            (fox hungry)))
+   (omnomnom
+    ((fox sated) rabbit rabbit rabbit rabbit rabbit
+     (fox hungry)))
+   (omnomnom
+    ((fox sated) (fox sated) rabbit rabbit rabbit rabbit))
+   (fox-multiply
+    ((fox sated) (fox sated) (fox hungry) rabbit rabbit rabbit
+     rabbit))
+   (omnomnom
+    ((fox sated) (fox sated) (fox sated) rabbit rabbit
+     rabbit)))))
 
 (run 1 (tr)
   (fresh (final rest diff)
