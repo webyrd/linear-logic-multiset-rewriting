53a7a3e6b361ff3f0b5114968ae2eb7226043a52
Chris Martens <chrisamaphone@gmail.com>
2018-02-05 14:46:22 -0500

    debug planning problem

--- blocksworld.scm
+++ blocksworld.scm
@@ -5,26 +5,33 @@
   (lambda (p q name)
     (conde
       [(fresh (block surface)
-          (== '((on ,block ,surface) (clear ,block) (free)) p)
-          (== '((holding ,block) (clear ,surface)) q)
-          (== 'pickup name))]
+          (== `((on ,block ,surface) (clear ,block) (free)) p)
+          (== `((holding ,block) (clear ,surface)) q)
+          (== `(pickup ,block) name))]
       [(fresh (block surface)
-          (== '((holding ,block) (clear ,surface)) q)
-          (== '((on ,block ,surface) (clear ,block) (free)) p)
-          (== 'putdown name))]
+          (== `((holding ,block) (clear ,surface)) p)
+          (== `((on ,block ,surface) (clear ,block) (free)) q)
+          (== `(putdown ,block ,surface) name))]
+      [   (== '() p)
+          (== '((clear table)) q)
+          (== 'clear-table name)]
      )))
 
 (define initial-state '((on a table) (on b a) (on c b) (clear c) (clear table) (free)))
 
-;;(define goal-state '((on a b) (on b c) (clear a)))
-(define goal-state '((holding c)))
+;; (define goal-state '((holding c)))
+;; (define goal-state '((on c table)))
+(define goal-state '((on a b) (on b c) (clear a)))
 
 #!eof
 
 
 (run 1 (tr)
   (fresh (final-state trace remainder-state)
-    (== `(,initial-state . ,trace) tr)
+    (step* initial-state final-state trace)
     (split goal-state final-state remainder-state)
-    (step* initial-state final-state trace)))
+    (== `(,initial-state . ,trace) tr)))
 
+(run* (rule1 state1 rule2 state2)
+    (step initial-state state1 rule1)
+    (step state1 state2 rule2))
